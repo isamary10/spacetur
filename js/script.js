35c70e503378415a881102bf140adbd7239b6cf2
Isa Mary <isa.kusuki@gmail.com>
2022-05-28 14:54:24 -0300

    Atualização do estilo do formulário e link para index no botão

--- js/script.js
+++ js/script.js
@@ -2,23 +2,22 @@ const botaoEnviar = document.getElementById("btnEnviar")
 
 botaoEnviar.addEventListener("click", validaCampos)
 
-function validaCampos(){
+function validaCampos() {
     const campos = document.querySelectorAll("input[type='text'],input[type='email'],input[type='date'], textarea")
 
-    for(let x = 0; x < campos.length ; x++){
-            if(campos[x].value == ""){
-                alert("O CAMPO " + campos[x].name + " está sem preenchimento!")
-                return
-            }
+    for (let x = 0; x < campos.length; x++) {
+        if (campos[x].value == "") {
+            alert("O CAMPO " + campos[x].name + " está sem preenchimento!")
+            return
+        }
     }
-
 }
 
-function radioSelecionado(){
+function radioSelecionado() {
     var res = '';
     const itens = document.getElementsByName('genero');
     for (var i = 0; i < itens.length; i++) {
-        if (itens[i].checked){
+        if (itens[i].checked) {
             res = itens[i].value
             break;
         }
