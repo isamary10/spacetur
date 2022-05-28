9a557c869349a2143d9c1ec51aae8c5a16e326bb
Isa Mary <isa.kusuki@gmail.com>
2022-05-28 16:47:52 -0300

    Atualização da validação do formulário

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+function radioSelecionado(){
+    var res = '';
+    const itens = document.getElementsByName('genero');
+    for (var i = 0; i < itens.length; i++) {
+        if (itens[i].checked){
+            res = itens[i].value
+            break;
+        }
+    }
+    return res;
+}
+
 function verificar() {
+    var res = radioSelecionado();
 
     var nome = document.getElementById("nmSobrenomeId")
     var email = document.getElementById("emailId")
@@ -13,6 +26,8 @@ function verificar() {
         alert("E-mail não informado")
     }else if(cpf.value == ""){
         alert("CPF não informado")
+    }else if (res === '') {
+        alert('Nenhum gênero selecionado')
     }else if(nascimento.value == ""){
         alert("Data de nascimento não informado")
     }else if(motivacao.value == ""){
@@ -21,7 +36,6 @@ function verificar() {
         alert("Dados cadastrados com sucesso!")
         link.href = "./index.html";
     }
-
 }
 
 
